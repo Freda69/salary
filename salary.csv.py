b1a4f9aff6c4fdc4ea4af4e771d8f8b452655564
Freda Ewurabena Buckson <bucksonfreda@gmail>
2019-09-03 04:21:03 -0700

    salary.csv.py folder

--- salary.csv.py
+++ salary.csv.py
@@ -5,3 +5,27 @@ Created on Mon Sep  2 04:49:23 2019
 @author: User
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib import style
+style.use('ggplot')
+
+dataset = pd.read_csv('salary.csv')
+X =  dataset.iloc[500: , :].values
+y = dataset.iloc[: , :1].values
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X =LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features=[0])
+
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 0)
+
+y_pred = predict(regressor, data.frame(Level = 1))
+
